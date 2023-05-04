6689b2adf8c926652cdcbee3c9e31283b4f45276
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2023-05-04 23:05:50 +0300

    Initial commit

---
+++ src/main/java/com/tbgdn/polar/catalog/PolarCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tbgdn.polar.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarCatalogServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PolarCatalogServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tbgdn/polar/catalog/PolarCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbgdn.polar.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarCatalogServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
